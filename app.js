bda7c0aef4b144f9e391ca1c4267acd3d6b26eee
Lucas <lucas@agtlucas.com>
2014-12-07 23:23:48 -0200

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,17 @@
 var koa = require('koa');
 var logger = require('koa-logger');
 var route = require('koa-route');
-var views = require('co-views');
-var parse = require('co-body');
+var todos = require('./controllers/todos');
 var app = koa();
+
+app.use(logger());
+
+app.use(route.get('/', todos.list));
+app.use(route.get('/todo/new', todos.add));
+app.use(route.get('/todo/:id', todos.show));
+app.use(route.get('/todo/delete/:id', todos.remove));
+app.use(route.get('/todo/edit/:id', todos.edit));
+app.use(route.post('/todo/create', todos.create));
+app.use(route.post('/todo/update', todos.update));
+
+app.listen(4000);
